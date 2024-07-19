ad5617c689170ad31f477bd41fbac83c3bedeb79
Puneet Tiwari <tiwaripuneet426@gmail.com>
2024-07-20 05:28:59 +0530

    Day 1 challenge completed

---
+++ Day1/Day1.js
@@ -0,0 +1,71 @@
+// task 1 
+var a = 1;
+console.log(a);
+
+//task 2
+
+let name = "Puneet"
+console.log(name)
+
+//task 3
+const bool1 = "True";
+const bool2 = "false";
+console.log(bool1,bool2)
+
+// task 4
+var num = 1;
+var str = "Puneet"
+var bool = "true"
+var arr = [1,23,4,"Puneet"]
+var obj = {
+    name: "Puneet",
+    age: 19,
+
+}
+console.log(num,str,bool,arr,obj)
+
+// task 5
+
+let name1 = "Puneet";
+console.log(name1)
+name1 = "Pulkit"
+console.log(name1)
+
+
+//task 6
+
+ const s = 12;
+//  s=13
+ console.log(s);
+
+ // Feature Request 1
+
+ var num = 1;
+var str = "Puneet"
+var bool = true
+var arr = [1,23,4,"Puneet"]
+var obj = {
+    name: "Puneet",
+    age: 19,
+}
+
+console.log(num, typeof(num))
+console.log(str,typeof(str))
+console.log(bool, typeof(bool))
+console.log(arr, typeof(arr))
+console.log(obj, typeof(obj))
+
+// Feature Request 2
+
+
+let city = "Ranchi"
+city = "Etawah"
+console.log(city)
+
+//let variables can be reassigned with different values
+
+const city1 = "Etawah"
+//city1 = "Ranchi"
+console.log(city1)
+
+//re-assignment to a constant variable is not allowed 
