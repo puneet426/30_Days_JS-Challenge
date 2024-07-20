e093b09a3e2feba2a36f562a913a05fbe0e78e58
Puneet Tiwari <tiwaripuneet426@gmail.com>
2024-07-20 06:44:40 +0530

    day 2 complete

--- Day2/operator.js
+++ Day2/operator.js
@@ -27,37 +27,61 @@ console.log(i%j)
 
 // Activity 2
 
-// task 7
+// task 6
 
 let num = 15
 num += 14
 console.log(num)
 
-// task 8
+// task 7
 let num1 = 15
 num1 -= 14
 console.log(num1)
 
 // Activity 3
 
-// task 9
+// task 8
 let num2 =42
 let num3 = 15
 const grea = num2>num3
 const small = num2<num3
 console.log(grea, small)
 
-// task 10 
+// task 9
 let num4 = 15
 let num5 = 15
 const grea1 = num4>=num5
 const small1 = num4<=num5
 console.log(grea1, small1)
 
-// TASK 11
+// TASK 10
 let num6 = 15
 let num7 = "15"
 const check1 = num6==num7
 const check2 = num6!=num7
 const check = num6===num7
 console.log(check,check1,check2)
+
+
+// Activity 4
+// task 11
+let num8 = 15
+let num9 = 10
+let comb = num8 && num9
+console.log(comb)
+
+// task 12
+
+let num10 = 15
+let num11 = 10
+let combOR = num10 || num11
+console.log(combOR)
+
+// task 13
+
+let num12 = 15
+let num13 = 10
+let combNOT = !num12 
+console.log(combNOT)
+
+
