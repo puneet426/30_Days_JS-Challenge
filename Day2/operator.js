be6840d3fd4f412069889e35c5b8aa50135728a5
Puneet Tiwari <tiwaripuneet426@gmail.com>
2024-07-20 06:11:45 +0530

    Day 2 challenge completed

---
+++ Day2/operator.js
@@ -0,0 +1,63 @@
+//Activity 1
+// task 1
+let a = 4
+let b = 5
+let sum = a+b
+console.log(sum)
+
+// task 2
+let c = 10
+let d = 2
+console.log(c-d)
+
+// task 3
+ let e = 15
+ let f = 3
+ console.log(e*f)
+
+ //task 4
+let g =70
+let h = 35
+console.log(g/h)
+
+// task 5
+let i = 10
+let j = 2
+console.log(i%j)
+
+// Activity 2
+
+// task 7
+
+let num = 15
+num += 14
+console.log(num)
+
+// task 8
+let num1 = 15
+num1 -= 14
+console.log(num1)
+
+// Activity 3
+
+// task 9
+let num2 =42
+let num3 = 15
+const grea = num2>num3
+const small = num2<num3
+console.log(grea, small)
+
+// task 10 
+let num4 = 15
+let num5 = 15
+const grea1 = num4>=num5
+const small1 = num4<=num5
+console.log(grea1, small1)
+
+// TASK 11
+let num6 = 15
+let num7 = "15"
+const check1 = num6==num7
+const check2 = num6!=num7
+const check = num6===num7
+console.log(check,check1,check2)
